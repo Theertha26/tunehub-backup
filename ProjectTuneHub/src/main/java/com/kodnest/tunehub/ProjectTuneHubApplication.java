66cb14f2743656bed33628eb2d9c639e1cc3d9b6
Theertha O <106685074+Theertha26@users.noreply.github.com>
2024-03-06 09:28:47 +0530

    Add files via upload

---
+++ ProjectTuneHub/src/main/java/com/kodnest/tunehub/ProjectTuneHubApplication.java
@@ -0,0 +1,15 @@
+package com.kodnest.tunehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTuneHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectTuneHubApplication.class, args);
+	}
+
+}
+//rzp_test_tavrEF24R9tybv
+//Tm1ypKX1DwF1J4K7OHlei2yo
